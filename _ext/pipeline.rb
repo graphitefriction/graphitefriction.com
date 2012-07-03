3f6eb8742d3b5a626f78eff452134476c91d69e5
Sarah White <graphitefriction@gmail.com>
2012-07-02 20:12:24 -0400

    finish setting up blog; add disqus comments; initial blog entries

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,6 +1,13 @@
 
 Awestruct::Extensions::Pipeline.new do
-  # extension Awestruct::Extensions::Posts.new( '/news' ) 
-  # extension Awestruct::Extensions::Indexifier.new
+  extension Awestruct::Extensions::Posts.new( '/blog' )
+  extension Awestruct::Extensions::Paginator.new( :posts, '/index', :per_page => 5 )
+  extension Awestruct::Extensions::Tagger.new( :posts, '/index', '/blog/tags', :per_page => 5 )
+  #extension Awestruct::Extensions::TagCloud.new( :posts, 'blog/tags/index.html')
+  extension Awestruct::Extensions::Atomizer.new( :posts, '/feed.atom' )
+  extension Awestruct::Extensions::Disqus.new
+  extension Awestruct::Extensions::Indexifier.new
+
+  helper Awestruct::Extensions::Partial
 end
 
