b038504d078e4e7c3dfa3199c77c73b40af15c82
Sarah White <graphitefriction@gmail.com>
2012-07-04 01:23:11 -0400

    add google analytics

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -9,5 +9,6 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Indexifier.new
 
   helper Awestruct::Extensions::Partial
+  helper Awestruct::Extensions::GoogleAnalytics
 end
 
