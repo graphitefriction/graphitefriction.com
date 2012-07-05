8c43bd9c0895a8d26f548c8c01736344b7268ec8
Sarah White <graphitefriction@gmail.com>
2012-07-05 18:54:12 -0400

    add supplamental offset to tz offset

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -2,7 +2,7 @@ require 'tz_offset'
 
 Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new( '/blog' )
-  extension Awestruct::Extensions::TzOffset.new( :posts, 'America/New_York' )
+  extension Awestruct::Extensions::TzOffset.new( :posts, 'America/New_York', 9.5 )
   extension Awestruct::Extensions::Paginator.new( :posts, '/index', :per_page => 5 )
   extension Awestruct::Extensions::Tagger.new( :posts, '/index', '/blog/tags', :per_page => 5 )
   #extension Awestruct::Extensions::TagCloud.new( :posts, 'blog/tags/index.html')
--- _ext/tz_offset.rb
+++ _ext/tz_offset.rb
@@ -3,15 +3,16 @@ require 'tzinfo'
 module Awestruct::Extensions
 
   class TzOffset
-    def initialize(entries_key = :posts, tz = 'UTC')
-      @offset_sec = TZInfo::Timezone.get(tz).period_for_local(Time.now()).utc_total_offset
+    def initialize(entries_key = :posts, tz = 'UTC', supplamental_offset = 0)
+      @tz_offset_sec = TZInfo::Timezone.get(tz).period_for_local(Time.now()).utc_total_offset
+      @supplamental_offset_sec = supplamental_offset * 3600
       @entries_key = entries_key
     end
 
     def execute(site)
       entries = site.send(@entries_key) || []
       entries.each do |entry|
-        entry.date = entry.date - @offset_sec
+        entry.date = entry.date - @tz_offset_sec + @supplamental_offset_sec
       end
     end
   end
