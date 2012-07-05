061b6ae1f0bce26cb1edcfb30ca83372eaea9a48
Sarah White <graphitefriction@gmail.com>
2012-07-05 18:58:54 -0400

    swap order of atomizer and indexifier extensions

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -6,9 +6,10 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Paginator.new( :posts, '/index', :per_page => 5 )
   extension Awestruct::Extensions::Tagger.new( :posts, '/index', '/blog/tags', :per_page => 5 )
   #extension Awestruct::Extensions::TagCloud.new( :posts, 'blog/tags/index.html')
+  # Indexifier *must* come before Atomizer
+  extension Awestruct::Extensions::Indexifier.new
   extension Awestruct::Extensions::Atomizer.new( :posts, '/feed.atom' )
   extension Awestruct::Extensions::Disqus.new
-  extension Awestruct::Extensions::Indexifier.new
 
   helper Awestruct::Extensions::Partial
   helper Awestruct::Extensions::GoogleAnalytics
